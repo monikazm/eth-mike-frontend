4aed62f21bdd07951487fbb0c663a6e8f9e00f43
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-04-15 14:55:57 +0200

    optimized startup time for the built exe file by stripping unneeded tkinter library and using Python optimizations

--- build.bat
+++ build.bat
@@ -1,16 +1,17 @@
 @echo off
+REM -- Create Python virtual environment if it does not exist
 IF NOT EXIST venv\ (
-@echo on
-python -m venv venv
-call venv\Scripts\activate.bat
-pip install -r requirements.txt
+    python -m venv venv
 )
 
 @echo on
+REM -- Activate virtual environment and ensure Python package dependencies are installed in it
 call venv\Scripts\activate.bat
-pyinstaller --onefile mike_simulator\main.py
+pip install -r requirements.txt
+
+REM -- Build self-contained exe file using PyInstaller
+python -OO -m PyInstaller --clean --noupx --exclude-module FixTk --exclude-module tcl --exclude-module tk --exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter --onefile mike_simulator\main.py
+
+REM -- Rename the created exe and move it to root directory
 copy dist\main.exe Simulator.exe
 rmdir /S /Q dist
-rmdir /S /Q mike_simulator\__pycache__
-rmdir /S /Q mike_simulator\assessment_modes\__pycache__
-rmdir /S /Q mike_simulator\input_backends\__pycache__
