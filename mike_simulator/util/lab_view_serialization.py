95dccd0a0391a3587dadc26cd4a50deecda4e413
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-04-21 15:33:46 +0200

    add functions for binary (de-)/serialization in labview format

---
+++ mike_simulator/util/lab_view_serialization.py
@@ -0,0 +1,48 @@
+import socket
+from dataclasses import fields
+from enum import IntEnum
+from typing import TypeVar, Type
+
+import netstruct
+
+# Helper structs which maps primitive types to the netstruct format string symbols which should be used to serialize
+# those types
+format_dict = {
+    bool: b'B',
+    float: b'f',
+    int: b'B', # for now all ints are assumed to be unsigned 8-bit values !!
+    str: b'i$'
+}
+
+T = TypeVar('T')
+
+
+def unflatten_from_string(sock: socket.socket, cls: Type[T]) -> T:
+    """
+    Unflatten data following the format of LabView's "Flatten to string" vi into a corresponding dataclass instance
+
+    :param sock: socket on which the (possibly variable-length) data is received
+    :param cls: type of the dataclass into which data should be deserialized
+    :return: dataclass instance
+    """
+
+    fmt, names = zip(*[(format_dict[field.type if not issubclass(field.type, IntEnum) else int], field.name) for field in fields(cls)])
+    unpacker = netstruct.obj_unpack(b''.join(fmt))
+    remain = unpacker.remaining
+    while (remain := unpacker.feed(sock.recv(remain))) != 0:
+        pass
+    vals = unpacker.result
+    return cls(**{name: (val.decode('utf-8') if isinstance(val, bytes) else val) for name, val in zip(names, vals)})
+
+
+def flatten_to_string(obj) -> bytes:
+    """
+    Flatten a dataclass instance into a binary representation which can be deserialized with LabView's "Unflatten from string" vi.
+
+    :param obj: dataclass instance to flatten
+    :return: binary string
+    """
+    fmt, vals = zip(*[(format_dict[field.type if not isinstance(field.type, IntEnum) else int], getattr(obj, field.name)) for field in fields(obj)])
+    vals = [val.encode('utf-8') if isinstance(val, str) else val for val in vals]
+    res = netstruct.pack(b''.join(fmt), *vals)
+    return res
